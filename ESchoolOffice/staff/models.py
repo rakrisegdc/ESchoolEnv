aa13715180a55a8d1124009569c90536ca716810
Segdc-Software <cindrellanithin@gmail.com>
2021-02-26 11:57:33 +0530

    designation model created

--- ESchoolOffice/staff/models.py
+++ ESchoolOffice/staff/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Designation(models.Model):
+    desig_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.desig_name
+
+
