424322738a6e06da15326634474cb445d401dd20
Segdc-Software <cindrellanithin@gmail.com>
2021-02-26 17:26:45 +0530

    staff app urls added

---
+++ ESchoolOffice/staff/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('designation/', views.designation()),
+    path('leavetype/', views.leavetype()),
+    path('staff/', views.staff()),
+    path('staffleave/', views.staffleave()),
+    path('teachersubjects', views.teachersubjects()),
+]
