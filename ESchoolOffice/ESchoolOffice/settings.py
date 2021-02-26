757f93e11553261045614d5d4dd746ae2c4348aa
rupeshpadmanabhan <rupeshpadmanabhan@gmail.com>
2021-02-26 14:01:25 +0530

    academics settings model class created

--- ESchoolOffice/ESchoolOffice/settings.py
+++ ESchoolOffice/ESchoolOffice/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'academic_settings.apps.AcademicSettingsConfig',
 ]
 
 MIDDLEWARE = [
--- ESchoolOffice/academic_settings/models.py
+++ ESchoolOffice/academic_settings/models.py
@@ -7,11 +7,20 @@ from django.utils import timezone
 class Subject(models.Model):
     sub_name = models.CharField(max_length=60)
 
+    def __str__(self):
+        return self.sub_name
+
 class Standard(models.Model):
     standard_name = models.IntegerField()
 
+    def __str__(self):
+        return self.standard_name
+
 class Division(models.Model):
     div_name = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.div_name
 
 class ClassDivision(models.Model):
     standard = models.ForeignKey(Standard, on_delete=models.RESTRICT)
@@ -19,16 +28,25 @@ class ClassDivision(models.Model):
 
 class AcademicYear(models.Model):
     acyear_year = models.CharField(max_length=9)
-    acyear_startdate = models.DateField(default=timezone.now())
-    acyear_enddate = models.DateField(default=timezone.now())
+    acyear_startdate = models.DateField(default=timezone.now)
+    acyear_enddate = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.acyear_year
 
 class ExamDetail(models.Model):
     exam_name = models.CharField(max_length=100)
     academicyear = models.ForeignKey(AcademicYear, on_delete=models.RESTRICT)
 
+    def __str__(self):
+        return self.exam_name
+
 class Grade(models.Model):
     grade_per = models.IntegerField()
     grade_grade = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.grade_grade
 
 class ClassSubject(models.Model):
     standard = models.ForeignKey(Standard, on_delete=models.RESTRICT)
@@ -42,7 +60,6 @@ class ExamMark(models.Model):
 
 class AcademicCalander(models.Model):
     academicyear = models.ForeignKey(AcademicYear, on_delete=models.RESTRICT)
-    accal_datefrom = models.DateField(default=timezone.now())
-    accal_dateto = models.DateField(default=timezone.now())
+    accal_datefrom = models.DateField(default=timezone.now)
+    accal_dateto = models.DateField(default=timezone.now)
     accal_particulars = models.CharField(max_length=1000)
-
