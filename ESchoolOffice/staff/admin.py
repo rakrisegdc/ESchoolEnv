c6a35cd8b1d1a172a8669e8092d2985965a512f7
Segdc-Software <cindrellanithin@gmail.com>
2021-02-26 14:15:12 +0530

    Created a DesignationAdmin class and registered this in admin.py

--- ESchoolOffice/staff/admin.py
+++ ESchoolOffice/staff/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class DesignationAdmin(admin.ModelAdmin):
+    fields = [ 'desig_name' ]
+
+
+admin.site.register(Designation, DesignationAdmin)
