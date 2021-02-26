69b3ada6aa1d8c76f0d29f1e6c0e1883349215ec
rupeshpadmanabhan <rupeshpadmanabhan@gmail.com>
2021-02-26 12:31:10 +0530

    academics settings model class created

---
+++ ESchoolOffice/academic_settings/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AcademicSettingsConfig(AppConfig):
+    name = 'academic_settings'
---
+++ ESchoolOffice/academic_settings/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Subject(models.Model):
+    sub_name = models.CharField(max_length=60)
+
+class Standard(models.Model):
+    standard_name = models.IntegerField()
+
+class Division(models.Model):
+    div_name = models.CharField(max_length=1)
+
+class ClassDivision(models.Model):
+    standard = models.ForeignKey(Standard, on_delete=models.RESTRICT)
+    division = models.ForeignKey(Division, on_delete=models.RESTRICT)
+
+class AcademicYear(models.Model):
+    acyear_year = models.CharField(max_length=9)
+    acyear_startdate = models.DateField(default=timezone.now())
+    acyear_enddate = models.DateField(default=timezone.now())
+
+class ExamDetail(models.Model):
+    exam_name = models.CharField(max_length=100)
+    academicyear = models.ForeignKey(AcademicYear, on_delete=models.RESTRICT)
+
+class Grade(models.Model):
+    grade_per = models.IntegerField()
+    grade_grade = models.CharField(max_length=2)
+
+class ClassSubject(models.Model):
+    standard = models.ForeignKey(Standard, on_delete=models.RESTRICT)
+    subject = models.ForeignKey(Subject, on_delete=models.RESTRICT)
+
+class ExamMark(models.Model):
+    examdetail = models.ForeignKey(ExamDetail, on_delete=models.RESTRICT)
+    classsubject = models.ForeignKey(ClassSubject, on_delete=models.RESTRICT)
+    exammark_max = models.IntegerField()
+    exammark_pass = models.IntegerField()
+
+class AcademicCalander(models.Model):
+    academicyear = models.ForeignKey(AcademicYear, on_delete=models.RESTRICT)
+    accal_datefrom = models.DateField(default=timezone.now())
+    accal_dateto = models.DateField(default=timezone.now())
+    accal_particulars = models.CharField(max_length=1000)
+
