836079355baee6c34fee20c4d0354f91f88747d7
Segdc-Software <cindrellanithin@gmail.com>
2021-02-26 17:22:30 +0530

    skeleton view func created

--- ESchoolOffice/staff/views.py
+++ ESchoolOffice/staff/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def designation(request):
+    return HttpResponse('Designation')
+
+
+def leavetype(request):
+    return HttpResponse('Leave Type')
+
+
+def staff(request):
+    return HttpResponse("Staff details")
+
+
+def staffleave(request):
+    return HttpResponse("Staff Leave details")
+
+
+def TeacherSubject(request):
+    return HttpResponse('Teacher Sub')
