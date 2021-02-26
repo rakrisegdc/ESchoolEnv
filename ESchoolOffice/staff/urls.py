3cca133435f5c92506582570d6bd3dfd5e89a00f
Segdc-Software <cindrellanithin@gmail.com>
2021-02-26 17:30:33 +0530

    typo corrected in staff app urls

--- ESchoolOffice/staff/urls.py
+++ ESchoolOffice/staff/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('designation/', views.designation()),
-    path('leavetype/', views.leavetype()),
-    path('staff/', views.staff()),
-    path('staffleave/', views.staffleave()),
-    path('teachersubjects', views.teachersubjects()),
+    path('designation/', views.designation),
+    path('leavetype/', views.leavetype),
+    path('staff/', views.staff),
+    path('staffleave/', views.staffleave),
+    path('teachersubjects', views.teachersubjects),
 ]
