ac839a451eab72dca287ac7b9939a01c11b0734d
Amruthasreeram <amruthasreeram@gmail.com>
2021-02-27 12:08:43 +0530

    wrote edit and delete functions for asset. Did a table view in html page for editing and updating

--- ESchoolOffice/asset/urls.py
+++ ESchoolOffice/asset/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import asset
+from .views import asset, edit_asset, delete_asset
 
 app_name = 'asset'
 urlpatterns = [
     url(r'^$', asset, name='AssetForms'),
+    url(r'edit_asset/(?P<pk>\d+)/$', edit_asset, name='edit_asset'),
+    url(r'delete_asset/(?P<pk>\d+)/$', delete_asset, name='delete_asset'),
 ]
--- ESchoolOffice/asset/views.py
+++ ESchoolOffice/asset/views.py
@@ -16,3 +16,27 @@ def asset(request):
     return render(request, 'asset/Asset.html', {'form': form, 'data': model_object})
 
 
+def delete_asset(request, pk):
+    post = get_object_or_404(Asset, pk=pk)
+    post.delete()
+    return redirect('asset:AssetForms')
+
+
+def edit_asset(request, pk):
+    template = "asset/asset.html"
+    post = get_object_or_404(Asset, pk=pk)
+    model_object = Asset.objects.all()
+    if request.method == 'POST':
+        form = forms.AssetForms(request.POST, instance=post)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            return redirect('asset:AssetForms')
+    else:
+        form = forms.AssetForms(instance=post)
+        context = {
+            'form': form,
+            'post': post,
+            'data': model_object,
+        }
+    return render(request, template, context)
