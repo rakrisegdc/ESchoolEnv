27aebb2ddbd1487a6eb747d9d80fbb8d5d8a4053
Segdc-Software <cindrellanithin@gmail.com>
2021-02-27 09:51:09 +0530

    url path specified

--- ESchoolOffice/staff/urls.py
+++ ESchoolOffice/staff/urls.py
@@ -1,11 +1,8 @@
+from django.conf.urls import url
 from django.urls import path
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('designation/', views.designation),
-    path('leavetype/', views.leavetype),
-    path('staff/', views.staff),
-    path('staffleave/', views.staffleave),
-    path('teachersubjects', views.teachersubjects),
+    path('designation/', designation),
 ]
