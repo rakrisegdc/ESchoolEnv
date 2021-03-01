3c2c721fce28fd890c31a2307a7076675ea291bb
rupeshpadmanabhan <rupeshpadmanabhan@gmail.com>
2021-03-01 14:10:38 +0530

    changes in admin registration

--- ESchoolOffice/academic_settings/admin.py
+++ ESchoolOffice/academic_settings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Subject, Standard, Division, AcademicYear, ExamDetail,Grade
 # Register your models here.
+
 admin.site.register(Subject)
 admin.site.register(Standard)
 admin.site.register(Division)
