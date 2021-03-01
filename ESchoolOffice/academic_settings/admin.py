dea7f94d949dc295ac77fb04922683900f7827e9
Rupeshkumar T V <79626291+rupeshsegdc@users.noreply.github.com>
2021-03-01 14:19:32 +0530

    Update admin.py
    same registraion of model resolved

--- ESchoolOffice/academic_settings/admin.py
+++ ESchoolOffice/academic_settings/admin.py
@@ -8,11 +8,3 @@ admin.site.register(Division)
 admin.site.register(AcademicYear)
 admin.site.register(ExamDetail)
 admin.site.register(Grade)
-
-from .models import *
-
-class StandardAdmin(admin.ModelAdmin):
-    fields = ['standard_name']
-
-admin.site.register(Standard, StandardAdmin)
-
