46194c1569a8fb5e6a8ca9dd86bf611744a53b1e
Athulya-T-Binu <athulyatbinu123@gmail.com>
2021-03-01 12:57:51 +0530

    added generic views for pta designation

--- ESchoolOffice/pta/urls.py
+++ ESchoolOffice/pta/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'polls'
+app_name = 'pta'
 urlpatterns = [
     path('des_create', views.PTADesignationForm.as_view(), name='des_create'),
-    path('des_index', views.PTADesignationIndexView.as_view(), name='des_index'),
-    path('des/<int:pk>/', views.PTADesignationDetailView.as_view(), name='des_detail'),
+    path('des_index', views.PTADesignationList.as_view(), name='des_index'),
+    path('des/<int:pk>/', views.PTADesignationView.as_view(), name='des_detail'),
     path('reg_create', views.CommiteeRegistrationForm.as_view(), name='reg_create'),
+    path('des_delete/<int:pk>/', views.PTADesignationDelete.as_view(), name='des_delete'),
+    path('des_update/<int:pk>/', views.PTADesignationUpdate.as_view(), name='des_update'),
 ]
