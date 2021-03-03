1dae35e467c16426af3328853e5e265f91180e7e
Sachusha <sachushasegdc@gmail.com>
2021-03-03 16:41:44 +0530

    html,admin.py,urls.py,views added

--- ESchoolOffice/static_settings/urls.py
+++ ESchoolOffice/static_settings/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'school_delete/(?P<pk>\d+)/$', school_delete, name='school_delete'),
     url(r'school_edit/(?P<pk>\d+)/$', school_edit, name='school_edit'),
     url(r'school_view/<int:pk>/', views.SchoolProfileView.as_view(), name='school_view'),
-    url(r'school_update/<int:pk>/', views.SchoolProfileupdate.as_view(), name='school_update'),
-
+    # url(r'school_update/<int:pk>/', views.SchoolProfileupdate.as_view(), name='school_update'),
+    # url(r'school_delete/<int:pk>/', views.SchoolProfileDelete.as_view(), name='school_delete'),
 ]
